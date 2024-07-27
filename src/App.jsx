cc5af0c5bac242bcb5988f270efc6b086a81e621
Muhammad Hasnain <hasnain0893@gmail.com>
2024-07-27 22:22:40 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,42 @@ import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 import Log from "./components/Log"
 
+function deriveActivePlayer(ganeTurns) {
+  let currentPlayer = 'X'
+  if (ganeTurns.length > 0 && ganeTurns[0].player === 'X') {
+    currentPlayer = 'O'
+  }
+  return currentPlayer;
+}
+
 function App() {
   const [ganeTurns, setGaneTurns] = useState([])
-  const [activePlayer, setActivePlayer] = useState('X')
+  // const [activePlayer, setActivePlayer] = useState('X')
 
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
-    setGaneTurns()
+  const activePlayer = deriveActivePlayer(ganeTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X')
+    setGaneTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns)
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: activePlayer }, ...prevTurns
+      ]
+      return updatedTurns
+    })
   }
 
   return (
-    <main id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player initialName='Hasnain' symbol='X' isActive={activePlayer === 'X'} />
-        <Player initialName='Ali' symbol='O' isActive={activePlayer === 'O'} />
-      </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
-      <Log />
-    </main>
+    <>
+      <main id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player initialName='Hasnain' symbol='X' isActive={activePlayer === 'X'} />
+          <Player initialName='Ali' symbol='O' isActive={activePlayer === 'O'} />
+        </ol>
+        <GameBoard turns={ganeTurns} onSelectSquare={handleSelectSquare} />
+      </main>
+      <Log turns={ganeTurns} />
+    </>
   )
 }
 
